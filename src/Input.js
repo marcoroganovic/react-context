f4832369702fb8f991b25a6424cd086363aba5ee
Marco Roganovic <marcoroganovic@gmail.com>
2018-07-29 18:53:58 +0200

    add basic styling for ul and li tags

--- src/Input.js
+++ src/Input.js
@@ -1,8 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import { connect } from "./Context";
 
+const StyledInput = styled.input`
+  display: block;
+  width: 50%;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #f0f0f0;
+`;
+
 const Input = ({ newTodo, handleChange }) => (
-  <input 
+  <StyledInput 
     type="text"
     placeholder="Do something..."
     value={newTodo}
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 import { connect } from "./Context";
+
+const Ul = styled.ul`
+  width: 50%;
+  margin: 20px auto;
+  padding: 0;
+  list-style: none;
+
+  li {
+    padding: 10px;
+    border-bottom: 1px solid #333;
+    cursor: pointer;
+  }
+`
 
 const TodoList = ({ todos, removeTodo }) => {
   const list = todos.map(todo => {
@@ -10,7 +24,7 @@ const TodoList = ({ todos, removeTodo }) => {
     );
   });
 
-  return <ul>{list}</ul>;
+  return <Ul>{list}</Ul>;
 }
 
 export default connect((state) => ({
